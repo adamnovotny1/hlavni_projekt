b72c13382dcd8bbae05881381a74717d19d34e18
adamnovotny1 <vasemailnagithub@gmail.com>
2025-02-17 14:10:32 +0100

    Přidání souboru login.html a vytvoření logiky pro přihlašování

--- main.py
+++ main.py
@@ -7,7 +7,16 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if username == 'admin' and password == 'heslo':
+            return render_template('login.html', login = True)
+        else:
+            return render_template('login.html', login = False)
+    return render_template('login.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
